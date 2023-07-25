911283e598477fb17a3d3e10eee82db372cf552b
Andrew Godwin <andrew@aeracode.org>
2023-07-24 18:37:35 -0600

    Provide way to create a superuser

--- docker/run.sh
+++ docker/run.sh
@@ -2,4 +2,8 @@ if [ -z "$SKIPMIGRATIONS" ]; then
     python manage.py migrate
 fi
 
-gunicorn herebedragons.wsgi:application -b 0.0.0.0:8000
+if [ ! -z "$DJANGO_SUPERUSER_PASSWORD" ]; then
+    python manage.py createsuperuser --username andrew --password "$SUPERUSERPASSWORD" --email andrew@aeracode.org --noinput
+fi
+
+gunicorn herebedragons.wsgi:application -b "0.0.0.0:$PORT"
